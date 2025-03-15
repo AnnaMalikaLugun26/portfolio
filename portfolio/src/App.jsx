98e93f8e594761e7155a0b05c2fac401ea5a2c32
Malikalugun <lugunannamalika26@gmail.com>
2025-03-15 09:43:04 +0530

    file updated
    file updated

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./assets/component/Navbar/Navbar";
+import Main from "./assets/component/Main/Main";
+function App() {
+  return (
+    <>
+      <Navbar />
+      {/* <Main /> */}
+    </>
+  );
+}
+
+export default App;
